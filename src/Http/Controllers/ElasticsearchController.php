62b889af771a2fbb6a5159bbcb92a900592e58e2
vanuffh7 <herbert.van-uffelen@univie.ac.at>
2022-10-15 17:01:35 +0200

    Upload to ES > 20000

--- src/Http/Controllers/ElasticsearchController.php
+++ src/Http/Controllers/ElasticsearchController.php
@@ -257,8 +257,8 @@ class ElasticsearchController extends Controller
 
     static function updateAllFieldsElasticSearch()  //only use for Bulk-Upload
     {
-        $i = 15000;
-        $max = 20000;
+        $i = 20000;
+        $max = 25000;
         $es = new Elasticsearch();
 
         $index = 'refs';
